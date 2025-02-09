18aeaaa393c04dc071b6cda7205a090710cfb6bb
tankb52 <tankb52@163.com>
2025-02-09 20:37:58 +0800

    20250209-203736：修复右键弹出系统菜单的问题。

--- waybar/right-click-menu.sh
+++ waybar/right-click-menu.sh
@@ -14,21 +14,21 @@
 
 # 定义菜单选项和对应命令
 menu_options=(
-  "  Settings"   "gnome-control-center"  # 示例：打开系统设置
-  "  Restart Bar"
-  "⏏  Wlogout" 
+  "  Settings"   "gnome-control-center"  # 示例：打开系统设置，名字需与下面的相同。
+  "  Restart waybar"
+  "⏏   Wlogout" 
   "  Reboot"
   "  Lock"
 )
 
 # 生成 wofi 菜单
-selected=$(printf "%s\n" "${menu_options[@]}" | wofi --dmenu -p "Menu" --lines 6)
+selected=$(printf "%s\n" "${menu_options[@]}" | wofi --dmenu -p "Menu" -W 15% --style ~/.config/wofi/style_arc-dark.css --lines 6)
 
 # 执行对应命令
 case "$selected" in
   "  Settings") gnome-control-center ;;
-  "  Restart Bar") killall waybar; sleep 1 && waybar & ;;
-  "⏏  Wlogout") "wlogout" ;;
+  "  Restart waybar") killall waybar; sleep 1 && waybar & ;;
+  "⏏   Wlogout") "wlogout" ;;
   "  Reboot") systemctl reboot ;;
   "  Lock") swaylock ;;
 esac
